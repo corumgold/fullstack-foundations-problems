7052eee37706897c5df07d0804070c41ab928cec
Corum Gold <goldcorum@gmail.com>
2022-06-27 22:30:02 -0400

    Solve trace the chain

--- traceTheChain.js
+++ traceTheChain.js
@@ -1,1 +1,10 @@
-
+const traceTheChain = (arg) => {
+    debugger;
+    let chain = [];
+    let proto = Object.getPrototypeOf(arg);
+    while (proto) {
+      if (proto) chain.push(proto);
+      proto = Object.getPrototypeOf(proto);
+    }
+    return chain;
+  };
